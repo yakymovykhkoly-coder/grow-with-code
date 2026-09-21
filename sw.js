3edc49d82ee78f23ba05ace80d0142d634031048
yakymovykhkoly-coder <yakymovykhkoly@gmail.com>
2026-09-21 13:56:10 +0300

    Refresh service worker cache strategy

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const C="gwc-v6";const A=["./","./index.html","./style.css","./config.js","./js/course-data.js","./js/course-ui.js","./js/community.js","./js/auth.js","./js/robot-city.js","./js/practice.js","./js/platform.js"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="gwc-v7";
+const A=["./","./index.html","./style.css","./config.js","./script.js","./manifest.webmanifest"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  const u=new URL(e.request.url);
+  if(u.pathname==="/config.js"||u.pathname==="/script.js"||u.pathname==="/sw.js"){
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
